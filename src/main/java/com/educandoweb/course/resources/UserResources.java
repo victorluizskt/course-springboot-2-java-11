4d672c7413c724872f319b3f0d3ae33040580c05
Victor Luiz <victorluizcefet@gmail.com>
2020-07-13 09:20:08 -0300

    H2 database, teste profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResources.java
+++ src/main/java/com/educandoweb/course/resources/UserResources.java
@@ -1,13 +1,14 @@
-package com.educandoweb.resources;
+package com.educandoweb.course.resources;
 
-import com.educandoweb.entities.User;
+import com.educandoweb.course.entities.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping(value = "/users")
+
 @RestController
+@RequestMapping(value = "/users")
 public class UserResources {
 
     @GetMapping
